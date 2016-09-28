711f11926aa169077588d34b4da6456f2df4f6a9
nicolassims <sableye.nico@gmail.com>
2016-09-28 15:04:17 -0400

    Figure out why return statement doubles closing message.

--- main.js
+++ main.js
@@ -8,23 +8,38 @@
 "use strict";
 const PROMPT = require('readline-sync');
 
-let policyNum, birthDate, premiumDueDate, numAccidents, customerAge, monthlyInsurancePremium;
+let policyNum, birthDate, premiumDueDate, numAccidents, customerAge, agePrice, monthlyInsurancePremium, continueInt;
 let lastName, firstName;
-const BASEPRICE = 100, AGE15TO30PRICE = 20, AGE30TO45PRICE = 10, AGE60PLUSPRICE = 30, ACCIDENTPRICE = 50;
 
 function main() {
-    setPolicyNum();
-    setBirthDate();
-    setPremiumDueDate();
-    setNumAccidents();
-    setCustomerAge();
-    //printTotalPrice();
+    if (continueInt == 1 || continueInt == null) {
+        setPolicyNum();
+        setLastName();
+        setFirstName();
+        setBirthDate();
+        setPremiumDueDate();
+        setNumAccidents();
+        setCustomerAge();
+        setAgePrice();
+        setMonthlyInsurancePremium();
+        printClosingRemark();
+        setContinueInt();
+    }
+    printGoodbye();
 }
 
 main();
 
 function setPolicyNum() {
     policyNum = PROMPT.question('Please enter the number of your insurance policy.\n>');
+}
+
+function setLastName() {
+    lastName = PROMPT.question('Please enter your last name.\n>');
+}
+
+function setFirstName() {
+    firstName = PROMPT.question('Please enter your first name.\n>');
 }
 
 function setBirthDate() {
@@ -36,19 +51,44 @@ function setPremiumDueDate() {
 }
 
 function setNumAccidents() {
-    numAccidents = PROMPT.question('How many accidents have you been at fault for in the past three years?\nDon\'t lie! >:-<\n>');
+    numAccidents = PROMPT.question('How many accidents have you been at fault for in the past three years?\n>');
 }
 
 function setCustomerAge() {
-    const SIXTYFIVEYEARS = 2051201880000, FORTYFIVEYEARS = 1420062840000, THIRTYYEARS = 946708560000, FIFTEENYEARS = 473354280000;
-    //Date.parse(birthDate) will return the number of milliseconds from that date since 1970, January 1st.
-    //Date.now() will return the number of milliseconds since 1970, January 1st.
-    customerAge = Date.parse(birthDate);
-    let dateToday = Date.now();
-    console.log(customerAge);
-    console.log(dateToday);
+    customerAge = Date.now() - Date.parse(birthDate);
 }
 
-//function printTotalPrice() {
-    //console.log('Your total price is $' + totalPrice + ', and your house will be built on lot #' + lotNum + '.');
-//}
+function setAgePrice() {
+    const SIXTYFIVEYEARS = 2051201880000, FORTYFIVEYEARS = 1420062840000, THIRTYYEARS = 946708560000,
+        FIFTEENYEARS = 473354280000, AGE15TO30PRICE = 20, AGE30TO45PRICE = 10, AGE60PLUSPRICE = 30;
+    if (customerAge > FIFTEENYEARS && customerAge < THIRTYYEARS) {
+        agePrice = AGE15TO30PRICE;
+    } else if (customerAge >= THIRTYYEARS && customerAge < FORTYFIVEYEARS) {
+        agePrice = AGE30TO45PRICE;
+    } else if (customerAge >= SIXTYFIVEYEARS) {
+        agePrice = AGE60PLUSPRICE;
+    } else {
+        agePrice = 0;
+    }
+}
+
+function setMonthlyInsurancePremium() {
+    const BASEPRICE = 100,  ACCIDENTPRICE = 50;
+    monthlyInsurancePremium = Number(BASEPRICE + agePrice + numAccidents * ACCIDENTPRICE);
+}
+
+function printClosingRemark() {
+    console.log('Thank you for using this terminal, ' + firstName + ' ' + lastName +'.\nYour total price is $'
+        + monthlyInsurancePremium + ', and your policy number is #' + policyNum + '.\nYour premium will be due on ' +
+        premiumDueDate + '.');
+}
+
+function setContinueInt() {
+    continueInt = PROMPT.question('Please enter a \'1\' if you want to continue using this program.\n' +
+        'If not, enter anything else.\n>');
+    return main();
+}
+
+function printGoodbye() {
+    console.log('Goodbye, and have a nice day.');
+}
